fed400855c77adac6f27d481b6d986ea2ba847fe
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2023-03-29 17:29:06 -0300

    commit de logger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'logger-test-proyect';
+
+  constructor(private logger: NGXLogger) {
+    this.logger.debug("Debug message");
+    this.logger.info("Info message");
+    this.logger.log("Default log message");
+    this.logger.warn("Warning message");
+    this.logger.error("Error message");
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LoggerModule,NgxLoggerLevel } from "ngx-logger";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { PruebaComponent } from './shared/compartido/prueba/prueba.component';
+import { PasswordComponent } from './shared/password/password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    PruebaComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoggerModule.forRoot({
+        serverLoggingUrl: '/api/logs',
+        level: NgxLoggerLevel.TRACE,
+        serverLogLevel: NgxLoggerLevel.ERROR,
+        disableConsoleLogging: false
+   })
   ],
   providers: [],
   bootstrap: [AppComponent]
